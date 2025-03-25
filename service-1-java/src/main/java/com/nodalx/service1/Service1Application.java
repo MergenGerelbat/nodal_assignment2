9e377650ac9cf89f3bfcfbbb3c8e3510daeea446
Mergen Gerelbat <mergen.gerelbat@gmail.com>
2025-03-25 16:33:39 -0500

    adding application files and pom files

---
+++ service-1-java/src/main/java/com/nodalx/service1/Service1Application.java
@@ -0,0 +1,14 @@
+package com.nodalx.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableScheduling
+public class Service1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service1Application.class, args);
+	}
+
+}
